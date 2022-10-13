ff5a2cee5fd85696ac64d2e819a769ebff668081
Matthew Leng <60058291+Mleng89@users.noreply.github.com>
2022-10-13 18:47:00 -0400

    Data: Models created

---
+++ TrackerLibrary/MatchupEntryModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace TrackerLibrary
+{
+    public class MatchupEntryModel
+    {
+        public TeamModel TeamCompeting { get; set; }
+        public double Score { get; set; }
+        public MatchupModel ParentMatchup { get; set; }
+    }
+}
+
---
+++ TrackerLibrary/TeamModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace TrackerLibrary
+{
+    public class TeamModel
+    {
+        public List<PersonModel> TeamMembers { get; set; } = new List<Person>();
+        public string TeamName { get; set; }
+
+       
+    }
+}
+
