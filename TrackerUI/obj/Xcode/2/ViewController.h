c59d8011bb45278a6a6c9f68bd7cdd3380772429
Matthew Leng <60058291+Mleng89@users.noreply.github.com>
2022-10-25 20:50:51 -0400

    FEAT: Added MacForms

---
+++ TrackerUI/obj/Xcode/2/ViewController.h
@@ -0,0 +1,18 @@
+// WARNING
+// This file has been generated automatically by Visual Studio to
+// mirror C# types. Changes in this file made by drag-connecting
+// from the UI designer will be synchronized back to C#, but
+// more complex manual changes may not transfer correctly.
+
+
+#import <Foundation/Foundation.h>
+#import <AppKit/AppKit.h>
+
+
+@interface ViewController : NSViewController {
+    __weak IBOutlet NSTextField *headerLabel;
+    __weak IBOutlet NSTextFieldCell *tournamentName;
+}
+- (IBAction)roundDropDown:(NSComboBox *)sender;
+
+@end
